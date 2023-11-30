eaed5b14884a9abb3a99557218c0946fbb1b9c78
Katharina Sick <katharina.sick@dynatrace.com>
2023-11-30 15:45:23 +0100

    feat: Install Scaffolder HTTP actions

--- packages/backend/src/plugins/scaffolder.ts
+++ packages/backend/src/plugins/scaffolder.ts
@@ -1,14 +1,28 @@
 import { CatalogClient } from '@backstage/catalog-client';
-import { createRouter } from '@backstage/plugin-scaffolder-backend';
+import {
+  createBuiltinActions,
+  createRouter,
+} from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import type { PluginEnvironment } from '../types';
+import { createHttpBackstageAction } from '@roadiehq/scaffolder-backend-module-http-request';
+import { ScmIntegrations } from '@backstage/integration';
 
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
-  const catalogClient = new CatalogClient({
-    discoveryApi: env.discovery,
-  });
+  const catalogClient = new CatalogClient({ discoveryApi: env.discovery });
+  const integrations = ScmIntegrations.fromConfig(env.config);
+
+  const actions = [
+    createHttpBackstageAction({ discovery: env.discovery }),
+    ...createBuiltinActions({
+      integrations,
+      catalogClient,
+      config: env.config,
+      reader: env.reader,
+    }),
+  ];
 
   return await createRouter({
     logger: env.logger,
@@ -18,5 +32,6 @@ export default async function createPlugin(
     catalogClient,
     identity: env.identity,
     permissions: env.permissions,
+    actions,
   });
 }
